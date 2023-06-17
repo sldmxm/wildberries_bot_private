ef63083021cbe4a881f0f15ff478273723bbedd3
artemii <roke.berezin@gmail.com>
2023-06-17 12:49:51 +0300

    research 1: найти и разработать решение для парсинга.

--- tgbot/bot/constants/text.py
+++ tgbot/bot/constants/text.py
@@ -8,3 +8,10 @@ START_MESSAGE = '''
 Привет! Чтобы воспользоваться ботом, нужно подписаться на наш
 <a href="https://t.me/mpexperts">telegram канал</a>.
 '''
+PRODUCT_POSITION_MESSAGE = '{city} - Место: {position} (стр. {page})\n'
+PRODUCT_POSITION_SCHEDULE_MESSAGE = (
+    '{city} - Место: {position}/{prev_position} '
+    '({position_arrow} {position_difference}) (стр. {page}/{prev_page} '
+    '({page_arrow} {page_difference}))'
+)
+PRODUCT_POSITION_NOT_FOUND_MESSAGE = '{city} - нет на первых 60 страниц\n'
