58bd2e5b1a3764aaa9f04fc413acefe1718d5a27
Александра Кайдалова <alexandrakaydalova@yandex.ru>
2023-08-02 11:44:32 +0300

    post review fix

--- tgbot/tgbot/settings.py
+++ tgbot/tgbot/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'tgbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': os.getenv('DB_ENGINE',
-#                            default='django.db.backends.postgresql_psycopg2'),
-#        'NAME': os.getenv('DB_NAME', default='postgres'),
-#        'USER': os.getenv('POSTGRES_USER', default='postgres'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-#        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-#        'PORT': os.getenv('DB_PORT', default='5432')
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': os.getenv('DB_ENGINE',
+                           default='django.db.backends.postgresql_psycopg2'),
+       'NAME': os.getenv('DB_NAME', default='postgres'),
+       'USER': os.getenv('POSTGRES_USER', default='postgres'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+       'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+       'PORT': os.getenv('DB_PORT', default='5432')
+   }
+}
 #
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
